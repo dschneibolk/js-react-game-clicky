0615f44ad08e5cc68a68f70da34dfcdd4081ed12
dschneibolk <dschneibolk@gmail.com>
2019-05-01 11:54:11 -0700

    Updates

---
+++ precache-manifest.4b7275a7010c2bc81a3bd827ec06dea2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96feb295c285d79563ba6552f02dd8fa",
+    "url": "/js-react-game-clicky/index.html"
+  },
+  {
+    "revision": "8b42b855acd923927dd2",
+    "url": "/js-react-game-clicky/static/css/main.2e6eca8c.chunk.css"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/js-react-game-clicky/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "8b42b855acd923927dd2",
+    "url": "/js-react-game-clicky/static/js/main.20972df1.chunk.js"
+  },
+  {
+    "revision": "4181edc3bd9a7937f069",
+    "url": "/js-react-game-clicky/static/js/runtime~main.308f3638.js"
+  }
+]);
